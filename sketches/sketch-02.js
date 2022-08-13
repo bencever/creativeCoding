814c8f9bec255ef3b13b52d45bf93feef1e82287
Bence Ver <szeraf@DESKTOP-TGFMFMV.localdomain>
2022-08-14 00:25:50 +0200

    inverted quarter

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -9,22 +9,24 @@ const settings = {
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = 'black';
     context.fillRect(0, 0, width, height);
 
-    context.fillStyle = 'black';
+    context.fillStyle = 'white';
+    context.strokeStyle = 'white';
 
-    const cx = width * 0.5;
-    const cy = height * 0.5;
+    const cx = 0 // width * 0.5;
+    const cy = 0 // height * 0.5;
 
     const w = width * 0.01;
     const h = height * 0.1;
     let x,y;
     
-    const num = 12;
-    const radius = width * 0.3;
+    const num = 120;
+    const radius = width * 0.7;
 
     for (let i = 0; i < num; i++) {
+      
       const slice = math.degToRad(360/num);
       const angle = slice * i;
 
@@ -34,14 +36,26 @@ const sketch = () => {
       context.save();
       context.translate(x,y);
       context.rotate(-angle);
-      context.scale(random.range(1,3),1);
+      context.scale(random.range(0.1,2),random.range(0.2,0.5));
 
       context.beginPath();
-      context.rect(-w*0.5,-h*0.5,w,h);
+      context.rect(-w*0.5,random.range(0,-h*0.5),w,h);
       context.fill();
+      context.restore();
+
+      context.lineWidth = random.range(5,20);
+
+      context.save();
+      context.translate(cx,cy);
+      context.rotate(-angle);
+
+      context.beginPath();
+      context.arc(0,0,radius * random.range(0.7,1.3),slice * random.range(1,-8),slice * random.range(1,5));
+      context.stroke();
       context.restore();
       
     }
+
 
   };
 };
